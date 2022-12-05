03e4dc337db6b6bda3cf0548200322f30d7ae903
Hossein Mamaghanian <amirhm@gmail.com>
2022-12-05 23:48:02 +0100

    day05

--- Day05/day05.py
+++ Day05/day05.py
@@ -1,35 +1,50 @@
 import re
-import copy
-filename = "data.txt"
+import sys
+from copy import deepcopy
 
 
-with open(filename) as fid:
-    txt = fid.read().split('\n')
-
-for idx, t in enumerate(txt):
-    if t.startswith('move'):
-        break
-
-brd, cmds = txt[:idx -1], txt[idx:]
-
-b = [[l[i:i+4].strip() for i in range(0, len(l), 4) ] for l in reversed(brd)]
-board = [[i for i in v if i] for v in  zip(*b)]
-brd = copy.deepcopy(board)
-
-for cm in cmds:
-    a, b, c = map(int, re.findall('move ([0-9]*) from ([0-9]*) to ([0-9]*)', cm)[0])
-    for k in range(a):
-        board[c - 1].append(board[b - 1].pop())
+reg = re.compile('move ([0-9]*) from ([0-9]*) to ([0-9]*)')
 
 
-part1 = "".join([l.pop().replace('[', "").replace(']', "") for l in board])
-print(part1)
+def readdata(filename):
+    with open(filename) as fid:
+        txt = fid.read().split('\n')
 
-board = brd
-for cm in cmds:
-    a, b, c = map(int, re.findall('move ([0-9]*) from ([0-9]*) to ([0-9]*)', cm)[0])
-    v = [board[b - 1].pop() for i in range(a)]
-    board[c - 1].extend(reversed(v))
+    for idx, t in enumerate(txt):
+        if t.startswith('move'):
+            break
+    brd, cmds = txt[:idx - 1], txt[idx:]
+    b = [[ln[i:i+4] for i in range(0, len(ln), 4)] for ln in reversed(brd)]
+    board = [[i for i in v if i.strip()] for v in zip(*b)]
+    return board, cmds
 
-part2 = "".join([l.pop().replace('[', "").replace(']', "") for l in board])
-print(part2)
+
+def part1(board, cmds):
+    for cm in cmds:
+        a, b, c = map(int, reg.findall(cm)[0])
+        for k in range(a):
+            board[c - 1].append(board[b - 1].pop())
+
+    ret = "".join([ln.pop() for ln in board])
+    return ret.replace('[', "").replace(']', "").replace(" ", "")
+
+
+def part2(board, cmds):
+    for cm in cmds:
+        a, b, c = map(int, reg.findall(cm)[0])
+        v = [board[b - 1].pop() for i in range(a)]
+        board[c - 1].extend(reversed(v))
+
+    ret = "".join([ln.pop() for ln in board])
+    return ret.replace('[', "").replace(']', "").replace(" ", "")
+
+
+def main():
+    _, filename, *_ = *sys.argv, "data.txt"
+    brd, cmds = readdata(filename)
+    print(part1(deepcopy(brd), cmds))
+    print(part2(brd, cmds))
+
+
+if __name__ == "__main__":
+    exit(main())
