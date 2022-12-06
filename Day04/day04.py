41f142ef7c726edbf56e47947d2c532525947e63
hmn <hossein.mamaghanian@advancedsilicon.com>
2022-12-06 13:13:41 +0100

    cleanup

--- Day04/day04.py
+++ Day04/day04.py
@@ -1,11 +1,13 @@
 import sys
 from collections import namedtuple
-points = namedtuple('points', ['start','stop'])
+points = namedtuple('points', ['start', 'stop'])
+
 
 def readdata(filename):
     with open(filename) as fid:
         txt = fid.read()
     return txt.split("\n")
+
 
 def part1(data):
     total = 0
@@ -14,9 +16,14 @@ def part1(data):
         p1 = points(*map(int, p1.split('-')))
         p2 = points(*map(int, p2.split('-')))
 
-        if ((p1.start <= p2.start) and (p1.stop >= p2.stop)) or ((p1.start >= p2.start) and (p1.stop <= p2.stop)):
+        if (
+            ((p1.start <= p2.start) and (p1.stop >= p2.stop))
+            or
+            ((p1.start >= p2.start) and (p1.stop <= p2.stop))
+           ):
             total += 1
     return total
+
 
 def part2(data):
     total = 0
@@ -25,9 +32,14 @@ def part2(data):
         p1 = points(*map(int, p1.split('-')))
         p2 = points(*map(int, p2.split('-')))
 
-        if ((p1[0] <= p2[0] <= p1[1]) or (p1[0] <= p2[1] <= p1[1])) or (p2[0] <= p1[0] <= p2[1])  : 
+        if (
+            ((p1[0] <= p2[0] <= p1[1]) or (p1[0] <= p2[1] <= p1[1]))
+            or
+            (p2[0] <= p1[0] <= p2[1])
+           ):
             total += 1
     return total
+
 
 def main():
     _, filename, *_ = *sys.argv, "data.txt"
