68ac072b59c7a33f1229fcb41f8226d41114c5ad
Hossein Mamaghanian <amirhm@gmail.com>
2022-12-14 12:52:37 +0100

    day 12 clean up

--- Day12/day12.py
+++ Day12/day12.py
@@ -1,8 +1,7 @@
 import sys
-import math
 from collections import namedtuple
 import heapq
-
+from itertools import product
 point = namedtuple('point', ['x', 'y'])
 
 
@@ -19,7 +18,7 @@ def neighbours(cord):
     yield point(cord.x, cord.y + 1)
 
 
-def move(data, path, target, seen):
+def move_fwd(data, path, target, seen):
     m, n = len(data), len(data[0])
     if path:
         cost, cord = heapq.heappop(path)
@@ -35,24 +34,48 @@ def move(data, path, target, seen):
                 if c not in seen:
                     seen.add(c)
                     heapq.heappush(path, (cost + 1, c))
-                    if c.x == target.x and c.y == target.y:
+                    if c in target:
                         return cost + 1
         return False
     else:
-        return 10000
+        raise AssertionError
+
+
+def move_bkd(data, path, target, seen):
+    m, n = len(data), len(data[0])
+    if path:
+        cost, cord = heapq.heappop(path)
+        for c in neighbours(cord):
+            if (
+                (0 <= c.x < m and 0 <= c.y < n) and
+                (
+                    (data[cord.x][cord.y] == "E") or
+                    (ord(data[cord.x][cord.y]) <= (ord(data[c.x][c.y]) + 1))
+                )
+            ):
+                if c not in seen:
+                    seen.add(c)
+                    heapq.heappush(path, (cost + 1, c))
+                    if c in target:
+                        return cost + 1
+        return False
+    else:
+        raise AssertionError
 
 
 def part1(lines):
     m, n = len(lines), len(lines[0])
-
-    start = [point(i, j) for i in range(m) for j in range(n) if lines[i][j] == "S"][0]
-    target = [point(i, j) for i in range(m) for j in range(n) if lines[i][j] == "E"][0]
+    itr = product(range(m), range(n))
+    start = [point(i, j) for i, j in itr if lines[i][j] == "S"][0]
+    itr = product(range(m), range(n))
+    target = [point(i, j) for i, j in itr if lines[i][j] == "E"][0]
+    target = set({target})
 
     def dj(start, target):
         path = [(0, start)]
         heapq.heapify(path)
         seen = set()
-        while not (cost := move(lines, path, target, seen)):
+        while not (cost := move_fwd(lines, path, target, seen)):
             pass
         return cost
 
@@ -63,18 +86,21 @@ def part1(lines):
 def part2(lines):
     m, n = len(lines), len(lines[0])
 
-    starts = [point(i, j) for i in range(m) for j in range(n) if lines[i][j] == "a"]
-    target = [point(i, j) for i in range(m) for j in range(n) if lines[i][j] == "E"][0]
+    itr = product(range(m), range(n))
+    starts = [point(i, j) for i, j in itr if lines[i][j] == "a"]
+    itr = product(range(m), range(n))
+    target = [point(i, j) for i, j in itr if lines[i][j] == "E"][0]
+    target, starts = set(starts), target
 
     def dj(start, target):
         path = [(0, start)]
         heapq.heapify(path)
         seen = set()
-        while not (cost := move(lines, path, target, seen)):
+        while not (cost := move_bkd(lines, path, target, seen)):
             pass
         return cost
 
-    return min([dj(start, target) for start in starts])
+    return dj(starts, target)
 
 
 def main():
